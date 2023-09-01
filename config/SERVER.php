144ccf3df6caa58b73467df63a3b69cf56759cce
Diosmar Arbey Cardenas Duran <diosmararbeycardenasduran@MacBook-Pro-de-Diosmar.local>
2023-08-31 19:59:34 -0500

    Database conexion constants added

---
+++ config/SERVER.php
@@ -0,0 +1,10 @@
+<?php
+const SERVER = "localhost";
+const DB = "micredito";
+const USER = "root";
+const PASS = "";
+
+//definicion constantes con parametros de encriptacion
+const METHOD ="AES-256-CBC";
+const KEY = '%GDIZEL@2023';
+const KEY2 = '240982';
