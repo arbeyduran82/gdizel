f8cbe401a2775ba037adba168264493f7a3a64bd
Diosmar Arbey Cardenas Duran <diosmararbeycardenasduran@MacBook-Pro-de-Diosmar.local>
2023-08-31 19:53:48 -0500

    Global constants are edited

--- config/APP.php
+++ config/APP.php
@@ -1,5 +1,5 @@
 <?php
-const SERVERURL="http://localhost/creditoapp/";
-const COMPANY ="Sistema Creditos";
+const SERVERURL="http://localhost/gdizel/";
+const COMPANY ="Sistema de gestion documental";
 const MONEDA = "$";
 date_default_timezone_set("America/Bogota");
